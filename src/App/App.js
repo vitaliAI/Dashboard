9a89f41b99f6c3bc4fca0cffac75abad78d83b4a
Vitali Mueller <vmueller@ltg.com>
2020-01-08 21:19:24 +0000

    Add AppProvider and hook up to AppBar

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Welcome from './WelcomeMessage'
 import AppLayout from './AppLayout'
 import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
 
 function App() {
   return (
       <AppLayout>
-          <AppBar/>
-           <Welcome />
+          <AppProvider>
+              <AppBar/>
+               <Welcome />
+          </AppProvider>
       </AppLayout>
   );
 }
